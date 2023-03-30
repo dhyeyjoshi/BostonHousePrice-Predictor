20ff9165fd97862c42be169855db455855e3d697
Dhyey Joshi <dhyey1974@gmail.com>
2023-03-31 01:02:32 +0530

    Correction in app.py file.

--- app.py
+++ app.py
@@ -36,10 +36,13 @@ def predict():
     data=[float(x) for x in request.form.values()]
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}$.".format(output))
+    output=regmodel.predict(final_input)
+    # return jsonify(output)
+    return render_template("home.html",prediction_text="The House price prediction is {}$.".format(output[0]))
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
    
      
